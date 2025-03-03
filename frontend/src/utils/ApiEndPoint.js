89187160a46d51080b3f40c7e752837be60a60b0
virbaba <virendersinghdev1@gmail.com>
2025-03-03 12:05:08 +0530

    change into api

--- frontend/src/utils/ApiEndPoint.js
+++ frontend/src/utils/ApiEndPoint.js
@@ -1,26 +1,18 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
-export const RECRUITER_API_END_POINT = "http://localhost:8000/api/v1/recruiter";
-export const ADMIN_API_END_POINT = "http://localhost:8000/api/v1/admin";
-export const ADMIN_STAT_API_END_POINT =
-  "http://localhost:8000/api/v1/admin/stat";
-export const ADMIN_USER_DATA_API_END_POINT =
-  "http://localhost:8000/api/v1/admin/user/data";
-export const ADMIN_COMPANY_DATA_API_END_POINT =
-  "http://localhost:8000/api/v1/admin/company/data";
-export const ADMIN_RECRUITER_DATA_API_END_POINT =
-  "http://localhost:8000/api/v1/admin/recruiter/data";
-export const ADMIN_JOB_DATA_API_END_POINT =
-  "http://localhost:8000/api/v1/admin/job/data";
-export const ADMIN_APPLICATION_DATA_API_END_POINT =
-  "http://localhost:8000/api/v1/admin/application/data";
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
-export const VERIFICATION_API_END_POINT =
-  "http://localhost:8000/api/v1/verification";
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
-export const ORDER_API_END_POINT = "http://localhost:8000/api/v1/order";
-export const APPLICATION_API_END_POINT =
-  "http://localhost:8000/api/v1/application";
-export const REVENUE_API_END_POINT = "http://localhost:8000/api/v1/revenue";
-export const NOTIFICATION_API_END_POINT =
-  "http://localhost:8000/api/v1/notifications";
-export const BACKEND_URL = "http://localhost:8000";
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+export const USER_API_END_POINT = `${BACKEND_URL}/api/v1/user`;
+export const RECRUITER_API_END_POINT = `${BACKEND_URL}/api/v1/recruiter`;
+export const ADMIN_API_END_POINT = `${BACKEND_URL}/api/v1/admin`;
+export const ADMIN_STAT_API_END_POINT = `${BACKEND_URL}/api/v1/admin/stat`;
+export const ADMIN_USER_DATA_API_END_POINT = `${BACKEND_URL}/api/v1/admin/user/data`;
+export const ADMIN_COMPANY_DATA_API_END_POINT = `${BACKEND_URL}/api/v1/admin/company/data`;
+export const ADMIN_RECRUITER_DATA_API_END_POINT = `${BACKEND_URL}/api/v1/admin/recruiter/data`;
+export const ADMIN_JOB_DATA_API_END_POINT = `${BACKEND_URL}/api/v1/admin/job/data`;
+export const ADMIN_APPLICATION_DATA_API_END_POINT = `${BACKEND_URL}/api/v1/admin/application/data`;
+export const COMPANY_API_END_POINT = `${BACKEND_URL}/api/v1/company`;
+export const VERIFICATION_API_END_POINT = `${BACKEND_URL}/api/v1/verification`;
+export const JOB_API_END_POINT = `${BACKEND_URL}/api/v1/job`;
+export const ORDER_API_END_POINT = `${BACKEND_URL}/api/v1/order`;
+export const APPLICATION_API_END_POINT = `${BACKEND_URL}/api/v1/application`;
+export const REVENUE_API_END_POINT = `${BACKEND_URL}/api/v1/revenue`;
+export const NOTIFICATION_API_END_POINT = `${BACKEND_URL}/api/v1/notifications`;
